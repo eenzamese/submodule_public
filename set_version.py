161690c6b71c5fd0198a8d176905936ca67b268b
Eenzamese <eenzamese@gmail.com>
2024-12-11 12:33:23 +0300

    Update set_version.py

--- set_version.py
+++ set_version.py
@@ -1,10 +1,11 @@
 import time
+import random
 print(time.ctime())
 with open('ms_version.txt', 'r') as version_file:
   version_content = version_file.read()
 print('CURRENT#########VERSION')
 print(version_content)
-version_value = str(int(time.time())) 
+version_value = str(random.randint(10000, 99999)) 
 new_version = version_content.replace('30920', version_value)
 print('NEW#########VERSION')
 with open('..\\ms_version_1.txt', 'w') as version_file:
