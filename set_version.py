aacf87effdc11db020cbad6f9a325984f01c8c30
Eenzamese <eenzamese@gmail.com>
2024-12-11 12:02:35 +0300

    Update set_version.py

--- set_version.py
+++ set_version.py
@@ -4,7 +4,8 @@ with open('ms_version.txt', 'r') as version_file:
   version_content = version_file.read()
 print('CURRENT#########VERSION')
 print(version_content)
-new_version = version_content.replace('30920', str(time.ctime()))
+version_value = str(int(time.time())) 
+new_version = version_content.replace('30920', version_value)
 print('NEW#########VERSION')
 with open('ms_version.txt', 'w') as version_file:
   version_file.write(new_version)
